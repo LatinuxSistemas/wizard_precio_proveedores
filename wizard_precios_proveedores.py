9b56a9f0031f95b452997560cbb972170d2688ff
gabrielo77 <gabrielfranciscodavini@gmail.com>
2013-06-12 16:41:45 -0300

    Update wizard_precios_proveedores.py

--- wizard_precios_proveedores.py
+++ wizard_precios_proveedores.py
@@ -31,66 +31,66 @@ class wizard_precios_proveedores(osv.osv_memory):
 	def onchange_suppliers(self,cr,uid,ids,provid):
 		res={}
 		if provid:
-			res['value']={'state':'choosed-prov'}
+			res['value'] = {'state':'choosed-prov'}
 		else:
-			res['value']={'state':'choosing'}		
+			res['value'] = {'state':'choosing'}		
 		return res
 			
 	def onchange_categories(self,cr,uid,ids,catid):
 		res={}
 		if catid:
-			res['value']={'state':'choosed-cat'}
+			res['value'] = {'state':'choosed-cat'}
 		else:
-			res['value']={'state':'choosing'}
+			res['value'] = {'state':'choosing'}
 		return res
 		
 	def onchange_products(self,cr,uid,ids,prodid):
 		res={}
 		if prodid:
-			res['value']={'state':'choosed-prod'}
+			res['value'] = {'state':'choosed-prod'}
 		else:
-			res['value']={'state':'choosing'}
+			res['value'] = {'state':'choosing'}
 		return res
 		
 	def accion(self,cr,uid,ids,context={}):
 		
 		this = self.browse(cr, uid, ids)		
-		tmpl= self.pool.get('product.template') 
-		prices=[]
-		tmplids=[]
-		hp=self.pool.get('historico.precios')
-		modificador=1+this[0]['average']/100				
+		tmpl = self.pool.get('product.template') 
+		prices = []
+		tmplids = []
+		hp = self.pool.get('historico.precios')
+		modificador = 1+this[0]['average']/100				
 		if this[0]['all_products']:			
-			tmplids=tmpl.search(cr,uid,[])			
+			tmplids = tmpl.search(cr,uid,[])			
 		elif this[0]['category_id']:
-			cid=this[0]['category_id'].id
-			tmplids=tmpl.search(cr,uid,[('categ_id','=',cid)])			
+			cid = this[0]['category_id'].id
+			tmplids = tmpl.search(cr,uid,[('categ_id','=',cid)])			
 		elif this[0]['proveedor']:
-			provid=this[0]['proveedor'].id
-			productos_proveedor=self.pool.get('product.supplierinfo')
-			productos_proveedor_ids=productos_proveedor.search(cr,uid,[('name','=',provid)])
-			templates=productos_proveedor.read(cr,uid,productos_proveedor_ids,['product_id'])			
+			provid = this[0]['proveedor'].id
+			productos_proveedor = self.pool.get('product.supplierinfo')
+			productos_proveedor_ids = productos_proveedor.search(cr,uid,[('name','=',provid)])
+			templates = productos_proveedor.read(cr,uid,productos_proveedor_ids,['product_id'])			
 			for t in templates:
 				tmplids.append(t['product_id'][0])
 		elif this[0]['product_id']:
-			prodid=this[0]['product_id'].id
+			prodid = this[0]['product_id'].id
 			template = self.pool.get('product.product').read(cr,uid,prodid,['product_tmpl_id'])		
 			tmplids.append(template['product_tmpl_id'][0])			
-		prices=tmpl.read(cr,uid,tmplids,['list_price','standard_price'])	
-		new_prices=[]		
-		new_price={}
+		prices = tmpl.read(cr,uid,tmplids,['list_price','standard_price'])	
+		new_prices = []		
+		new_price = {}
 		if this[0]['standard_price'] or this[0]['list_price']: 
 			for dic in prices:
 				new_price['id']=dic['id']
 				if this[0]['standard_price']:
-					new_price['standard_price']=dic['standard_price']*modificador
-					new_price['list_price']=dic['list_price']
+					new_price['standard_price'] = dic['standard_price']*modificador
+					new_price['list_price'] = dic['list_price']
 				elif this[0]['list_price']:
-					new_price['list_price']=dic['list_price']*modificador
-					new_price['standard_price']=dic['standard_price']
+					new_price['list_price'] = dic['list_price']*modificador
+					new_price['standard_price'] = dic['standard_price']
 				else:
-					new_price['standard_price']=dic['standard_price']
-					new_price['list_price']=dic['list_price']
+					new_price['standard_price'] = dic['standard_price']
+					new_price['list_price'] = dic['list_price']
 				new_prices.append(new_price)
 				tmpl.write(cr,uid,new_price['id'],{'standard_price':new_price['standard_price'],'list_price':new_price['list_price']},context={})
 				#hp.create(cr,uid,{'tmpl_id':new_price['id'], 'standard_price':new_price['standard_price'], 'list_price':new_price['list_price'], 'fecha_y_hora': strftime("%d/%m/%Y - %H:%M:%S")})
